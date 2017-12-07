d5fb791cb80b9009a7e7fe195f401090585edca2
Takaaki Suzuki <taa-suzuki@kddi.com>
2017-12-07 16:27:14 +0900

    modify java9 to java

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ echo "--- LAUNCH ---"
 echo " > run monitor"
 echo ""
 
-# the classpath is needed for Spark's dependencies
-java9 \
+# the classpath is needjed for Spark's dependencies
+java \
 	--class-path 'build/class-path/*' \
 	--module-path build/module-path \
 	--add-modules java.xml.bind \
